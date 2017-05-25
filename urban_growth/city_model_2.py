235ec5bafca38d118f388743e2ce493fc38423b2
Philip Samuel Chodrow <pchodrow@gmail.com>
2017-05-25 11:23:47 -0400

    compute settlement probability densities

--- urban_growth/city_model_2.py
+++ urban_growth/city_model_2.py
@@ -1,31 +1,75 @@
 import numpy as np
 from skimage import morphology
 from scipy.spatial import distance
-
+from scipy.special import expit
+from scipy.ndimage.filters import gaussian_filter
 
 class settlement_model:
 
-	def __init__(self, M0 = None, geo = None, D = None, L = None):
-
-		self.M0 = M0
+	def __init__(self, M0 = None, geo = None, D = None):
 
 		if geo is not None:
 			self.geo = geo
-		else:
+		elif M0 is not None:
 			geo = np.ones(self.M0.shape)
 
 		if D is not None: 
 			self.D = D
 
-	def simulate(self, model_type, pars, return_trace = True, trace_frequency = 1, trace_dir = ''):
-		print 'not implemented'
+	def set_M0(self, M0 = None, **kwargs):
+		if M0 is not None:
+			self.M0 = M0
+		else:
+			self.M0 = random_mat(**kwargs)
+
+	def get_M0(self):
+		return self.M0
+
+	def settlement_type_matrix(self, thresh):
+		rural, urban, unsettled = settlement_types(self.M0, thresh = thresh)
+		types = np.zeros(self.M0.shape)
+
+		for i in range(urban.shape[0]):
+		    types[tuple(urban[i,])] =  1
+		for i in range(rural.shape[0]):
+		    types[tuple(rural[i,])] = -1		
+
+		return types
+
+	def density(self, thresh, pars):
+		'''
+			Forward step
+		'''
+
+
+		dists = get_dists(self.M0, thresh)
+		
+		gammas = ('gamma_r', 'gamma_u')
+		g_pars = {k: pars[k] for k in gammas}
+		p_pars = {k: pars[k] for k in pars if k not in gammas}
+
+		w_rural, w_urban = distance_weights(self.M0, dists, thresh, **g_pars) # focus on transforming into appropriate array
+		# probs = model_1_probs(dists, "other stuff") # not implemented, should be 2 L times L 	arrays
+		
+		return model_1_probs(w_rural, w_urban, **p_pars)
 
 	def infer(self, model_type, M1, return_trace = True):
+		'''
+			Backward step
+		'''
 		print 'not implemented'
 
-def id_clusters(M, **kwargs):
+def model_1_probs(w_rural, w_urban, alpha_r, beta_r, alpha_u, beta_u):
+	p_r = expit(alpha_r * w_rural + beta_r)
+	p_u = expit(alpha_u * w_urban + beta_u)
 
-	mask = morphology.label(M, **kwargs)
+	denom = p_r + p_u
+
+	return p_r ** 2 / denom, p_u ** 2 / denom
+
+def id_clusters(M):
+
+	mask = morphology.label(M, connectivity = 1)
 	return mask
 
 def cluster_areas(mask):
@@ -35,12 +79,12 @@ def cluster_areas(mask):
 	area_dict.pop(0) # ignore background
 	return area_dict
 
-def settlement_types(M,  return_type = 'cell', thresh = 0, **kwargs):
+def settlement_types(M,  return_type = 'cell', thresh = 0):
 	'''
 		Compute the types of cells and return the results at either the cluster or cell level. 
 	'''
 
-	mask      = id_clusters(M, **kwargs)
+	mask      = id_clusters(M)
 	area_dict = cluster_areas(mask)
 	
 	urban_clusters  = {key : area_dict[key] for key in area_dict if area_dict[key] >= thresh}
@@ -67,13 +111,15 @@ def settlement_types(M,  return_type = 'cell', thresh = 0, **kwargs):
 
 		return rural_cells, urban_cells, unsettled_cells
 
-def get_dists(M, thresh = 0, **kwargs):
+# Distance computations. An alternative way to approach this would be to compute the distance to the centroid weighted by the size of the cluster, which would have slightly different physics but would likely be MUCH faster. Would need to check for weird behavior -- as cluster gets larger, weight should increase, but this is not prima facie guaranteed under the centroid scheme. 
+
+def get_dists(M, thresh = 0):
 	
 	def f(j, arr):
 		a = np.unique(arr[j,], return_counts=True)
 		return [(a[0][i], a[1][i]) for i in range(a[0].shape[0])]
 
-	rural, urban, unsettled = settlement_types(M, 'cell', thresh, **kwargs)
+	rural, urban, unsettled = settlement_types(M, 'cell', thresh)
 	
 	rural_dist_mat = distance.cdist(unsettled, rural, 'euclidean').astype(int)
 	rural_dists    = {i : f(i, rural_dist_mat) for i in range(rural_dist_mat.shape[0])}
@@ -83,7 +129,7 @@ def get_dists(M, thresh = 0, **kwargs):
 
 	return rural_dists, urban_dists
 
-def distance_weights(M, dists, gamma_r, gamma_u, thresh, **kwargs):
+def distance_weights(M, dists, thresh, gamma_r, gamma_u):
 	def f(i, gamma_r, gamma_u):
 		
 		a = np.array(dists[0][i])
@@ -94,10 +140,12 @@ def distance_weights(M, dists, gamma_r, gamma_u, thresh, **kwargs):
 		
 		return r, u
 	
-	rural, urban, unsettled = settlement_types(M, 'cell', thresh, **kwargs)
+	rural, urban, unsettled = settlement_types(M, 'cell', thresh)
 	
-	weights_rural = np.zeros(M.shape)
-	weights_urban = np.zeros(M.shape)
+	weights_rural = np.empty(M.shape)
+	weights_urban = np.empty(M.shape)
+	weights_rural.fill(np.nan)
+	weights_urban.fill(np.nan)
 
 	weight_tuples = [f(i, gamma_r, gamma_u) for i in range(len(dists[0]))]
 
@@ -107,7 +155,33 @@ def distance_weights(M, dists, gamma_r, gamma_u, thresh, **kwargs):
 
 	return weights_rural, weights_urban
 
+# def model_1_probs(M, gamma_r, gamma_u, alpha_r, alpha_u, beta_r, beta_u, thresh):
 
+# 	dists = get_dists(M, thresh)
+# 	weights_rural, weights_urban = distance_weights(M, dists, gamma_r, gamma_u, thresh)
 
+# 	p_r = alpha_r * weights_rural + beta_r 
+# 	p_u = alpha_u * weights_urban + beta_u 
 
+# 	denom = p_r + p_u
+
+# 	return p_r ** 2 / denom, p_u ** 2 / denom
+
+def random_mat(L, density = .5, blurred = True, blur = 3, central_city = True):
+    
+    M = np.random.rand(L, L)
+
+    if central_city:
+
+        M[(1 * L/2 - L / 10):(1 * L/2 + L/10),(1 * L/2 - L / 10):(1 * L/2 + L/10)] = 0
+
+    if blurred: 
+        M = gaussian_filter(M, blur)
+        
+    ix_low = M < density  # Where values are low
+    M[ix_low]  = 1
+
+    M[M < 1] = 0
+        
+    return M
 

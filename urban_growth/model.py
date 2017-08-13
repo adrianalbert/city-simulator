c036f2e277ae7ff9894e87fda653de6ffa1e42cd
Phil Chodrow <pchodrow@mit.edu>
2017-08-13 13:29:03 -0400

    collected mods from server

--- urban_growth/model.py
+++ urban_growth/model.py
@@ -21,7 +21,7 @@ class settlement_model:
 # INITIALIZATION
 ################################################################################
 
-	def __init__(self, M0 = None, geo = None,  N_pix = 100, sigma = 1, t = .5):
+	def __init__(self, M0 = None, geo = None,  N_pix = 100, sigma = 1, t = .5, class_type = 'blur', thresh = 10):
 		if M0 is not None:
 			self.set_M0(M0 = M0)
 		if geo is not None:
@@ -32,7 +32,8 @@ class settlement_model:
 		self.N_pix = N_pix
 		self.sigma = sigma
 		self.t     = t
-
+		self.class_type = class_type
+		self.thresh = thresh
 		self.partition_clusters()
 		
 	def set_M0(self, M0 = None, **kwargs):
@@ -47,11 +48,18 @@ class settlement_model:
 		return self.M0
 
 	def partition_clusters(self):
-		self.C = gaussian_blur_partition(self.M, self.sigma, self.t)
+		if self.class_type == 'blur':
+			self.C = gaussian_blur_partition(self.M, self.sigma, self.t)
+		# the below is pseudocode
+		elif self.class_type == 'thresh':
+			self.C = threshold_partition(self.M, self.thresh)
+			
+			
 
+			
 	def settlement_rate(self, K, pars, use_grad = False):
 		'''
-		Wasteful to compute the gradient information when we don't need it. 
+		Wasteful to compute the gradient information when we do not need it. 
 		Possible improvement needed for when we get to simulating. Should 
 		only be a 2x speedup though, so may not be worth the time. 
 		'''
--- urban_growth/utility.py
+++ urban_growth/utility.py
@@ -45,28 +45,47 @@ def normalizer(gamma, extent = 50, deriv = False):
 	return norm
 
 def distance_kernel(L, base_val = .5, unit = 1):
-    
-    M = np.zeros((2*L + 1, 2*L + 1))
-    ix_M = np.where(M > -1)
-    ix_arr_M = np.array([ix_M[0], ix_M[1]]).T / unit
-    dists = distance.cdist(ix_arr_M, np.array([[L, L]]))
-    M = dists.reshape(M.shape)
-    M[L, L] = base_val
-    return M
+	
+	M = np.zeros((2*L + 1, 2*L + 1))
+	ix_M = np.where(M > -1)
+	ix_arr_M = np.array([ix_M[0], ix_M[1]]).T / unit
+	dists = distance.cdist(ix_arr_M, np.array([[L, L]]))
+	M = dists.reshape(M.shape)
+	M[L, L] = base_val
+	return M
 	
 def gaussian_blur_partition(M, sigma, t):
-	m         = gaussian_filter(M, sigma)
+	m    = gaussian_filter(M, sigma)
 	C    = np.zeros((2, m.shape[0], m.shape[1]))
 	C[0] = (m < t)  * M  # rural
 	C[1] = (m >= t) * M # urban
 	return C
 
+def threshold_partition(M, thresh):
+	
+	morph  = morphology.label(M > 0)
+	C      = np.zeros((2, morph.shape[0], morph.shape[1]))
+	labels = np.unique(morph)
+	sizes  = {lab : M[np.where(morph == lab)].sum() for lab in labels}
+
+	for lab in sizes:
+		if sizes[lab] >= thresh:
+			C[1][np.where(morph == lab)] = 1
+		else:
+			C[0][np.where(morph == lab)] = 1
+	
+	return C
+			
+			
+
+
+
 def to_vec(pars):
-    return np.concatenate((pars['alpha'], pars['gamma'], np.array([pars['beta']])))
+	return np.concatenate((pars['alpha'], pars['gamma'], np.array([pars['beta']])))
 
 def from_vec(pars_vec):
-    d = {'alpha' : pars_vec[0:2],
-         'gamma' : pars_vec[2:4],
-         'beta'  : pars_vec[4]}
-    return d
-    
+	d = {'alpha' : pars_vec[0:2],
+		 'gamma' : pars_vec[2:4],
+		 'beta'  : pars_vec[4]}
+	return d
+	
